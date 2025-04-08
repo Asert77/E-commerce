48691dfe2d4097eff7a89503734fc34ff11a9076
ismail090106 <ismailabduganiew@gmail.com>
2025-04-08 16:30:02 +0500

    O'zgartirishlar qilindi

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.181.177.129', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
