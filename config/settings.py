a285a64a69629952f72ecd1ce4440ee6593ab281
ismail090106 <ismailabduganiew@gmail.com>
2025-06-25 18:57:36 +0500

    Frontend uchun o'zgartirishlar qilindi

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["http://54.238.152.180"]
+ALLOWED_HOSTS = ["http://54.238.152.180", "localohost", "127.0.0.1"]
 
 
 # Application definition
